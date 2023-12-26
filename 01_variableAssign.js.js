a03724e13a77e7f1733554f809bde0a0b2a82c32
geetesh19 <136695821+geetesh19@users.noreply.github.com>
2023-12-26 12:02:44 +0530

    Variable assignment
    Variable assignment

---
+++ 01_variableAssign.js.js
@@ -0,0 +1,50 @@
+
+
+var myFullName
+console.log(myFullName);
+
+myFullName = "Geetesh Jagzape"
+console.log("Full Name:",myFullName);
+
+myFullName = "Geetesh Ravindra Jagzape"
+console.log("Full Name:",myFullName);
+
+var myLove
+myLove = "I love only JS"
+console.log(myLove);
+
+var firstName 
+firstName ="Geetesh"
+console.log("First Name:",firstName);
+
+var lastName
+lastName ="Jagzape"
+console.log("Last Name:",lastName);
+
+var age
+age = "25"
+console.log("Age :",age);
+
+var address
+address ="Pune"
+console.log("Address :",address);
+
+var pincode
+pincode = "411057"
+console.log("Pincode :",pincode);
+
+var gender
+gender = "Male"
+console.log("Gender :",gender);
+
+var isMarried
+isMarried ="No"
+console.log("Is Married :",isMarried);
+
+
+
+
+
+
+
+ 
